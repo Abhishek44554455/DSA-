aeb7e8a1d9e3e05e882c75880188a7701d72fe54
Abhishek Anand <sauravk8825@gmail.com>
2026-03-15 19:47:04 +0530

    I learn about pointer and how it help to store the address in memory location elaborate it so that i post it in linkedin

--- pointers/day3/intArr.cpp
+++ pointers/day3/intArr.cpp
@@ -9,6 +9,10 @@ void dbl(int *ptr){
 
 int main(){
     int arr[5]={1,2,3,4,5};
+    int *p=arr;
+    for(int i=0;i<5;i++){
+        cout<<(*p+i+1)<<endl;
+    }
     dbl(arr);
     for(auto x:arr){
         cout<<x;
--- pointers/day4/doublePointer.cpp
+++ pointers/day4/doublePointer.cpp
@@ -1,9 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
+// int main(){
+//     int n=10;
+//     int *p=&n;
+//     cout<<p<<endl;
+//     int **ptr=&p;
+//     cout<<*ptr;
+// }
+
+// void fun(int *p1){
+//     cout<<p1;
+// }
+// int main(){
+//     int n=10;
+//     int *p=&n;
+//     cout<<p<<endl;
+//     fun(p);
+
+// }
+// int main(){
+//     int n=10;
+//     int *p=&n;
+//     int **ptr=&p;
+//     cout<<p<<endl;
+//     cout<<*ptr+1<<endl;
+// }
+
+// int main(){
+//     int n=10;
+//     int &temp=n;
+//     cout<<&n;
+//     cout<<(&temp+1);
+// }
+
+//change the value to 15 using three pointer
+
 int main(){
     int n=10;
-    int *p=&n;
-    cout<<p<<endl;
-    int **ptr=&p;
-    cout<<*ptr;
+    int *p1=&n;
+    int **p2=&p1;
+    int ***p3=&p2;
+    cout<<(***p3)+5<<endl;
+
 }
